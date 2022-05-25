74585065e68cf64223bc87738c60fc1ccc660632
matborowiak <m.borowiak@hotmail.com>
2022-05-25 19:20:08 +0200

    clean up

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,13 @@
 import { setResultStyle } from '../utils/setResultStyle'
 
-export const Result = ({ charactersToRender }) => {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      {charactersToRender.map(
-        ({ guessCharacter, exists, isInPosition }, index) => (
-          <h1 key={index} style={setResultStyle(exists, isInPosition)}>
-            {guessCharacter}
-          </h1>
-        )
-      )}
-    </div>
-  )
-}
+export const Result = ({ charactersToRender }) => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    {charactersToRender.map(
+      ({ guessCharacter, exists, isInPosition }, index) => (
+        <h1 key={index} style={setResultStyle(exists, isInPosition)}>
+          {guessCharacter}
+        </h1>
+      )
+    )}
+  </div>
+)
--- src/components/ResultHistory.js
+++ src/components/ResultHistory.js
@@ -1,9 +1,6 @@
-import { useEffect } from 'react'
 import { Result } from './Result'
 
-export const ResultHistory = ({ storedGuesses }) => {
-  console.log({ storedGuesses })
-  return storedGuesses.map((charactersToRender, index) => {
+export const ResultHistory = ({ storedGuesses }) =>
+  storedGuesses.map((charactersToRender, index) => {
     return <Result key={index} charactersToRender={charactersToRender} />
   })
-}
